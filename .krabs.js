a4b676062a0d60d23ab7ea247b5d18f35b89ed18
unknown <shasheen@i-threesixty.com>
2022-08-04 14:42:45 +0530

    refactor krab file add heroku url

--- .krabs.js
+++ .krabs.js
@@ -6,8 +6,7 @@ module.exports = {
         {
           development: /[a-z]*\.modernwalk\.com/,
           stage: "stage.modernwalk.com",
-          production:
-            /[a-z]*\.modernwalkmultitenant-env\.eba-wp83tjsx.us-west-2\.elasticbeanstalk\.com/, // Regex supported!
+          production: /[a-z]*\.modernwalk-krab\.herokuapp\.com/, // Regex supported!
         },
       ],
     },
