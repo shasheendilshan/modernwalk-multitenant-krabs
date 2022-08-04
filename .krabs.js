038af76f9119fb812bbcc143517e2568076ae832
unknown <shasheen@i-threesixty.com>
2022-08-04 10:57:55 +0530

    chor change krab dev and prod url

--- .krabs.js
+++ .krabs.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "modernwalk",
       domains: [
         {
-          development: /[a-z]*\.modernwalk\.com/,
+          development: /[a-z]*\.modernwalk-krab.herokuapp.com/,
           stage: "stage.modernwalk.com",
           production: /[a-z]*\.modernwalk-krab.herokuapp.com/, // Regex supported!
         },
